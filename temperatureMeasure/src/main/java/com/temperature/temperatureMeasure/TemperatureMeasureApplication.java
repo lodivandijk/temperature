44db54c68e0715762f50b3249ec31e7e4b594313
lodivandijk <49106018+lodivandijk@users.noreply.github.com>
2023-10-08 20:53:34 +0100

    Add files via upload

---
+++ temperatureMeasure/src/main/java/com/temperature/temperatureMeasure/TemperatureMeasureApplication.java
@@ -0,0 +1,13 @@
+package com.temperature.temperatureMeasure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureMeasureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureMeasureApplication.class, args);
+	}
+
+}
---
+++ temperatureMeasure/src/test/java/com/temperature/temperatureMeasure/TemperatureMeasureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temperature.temperatureMeasure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureMeasureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
